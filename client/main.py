5bb820c513bf4b4187c866c5b496f41ee80dbe46
BohdanZasymovych <bohdan.zasymovych@gmail.com>
2025-05-30 16:38:39 +0300

    Websocket connection implemented

--- client/main.py
+++ client/main.py
@@ -1,8 +1,21 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from gui import MineMap
+# import sys
+# from PyQt5.QtWidgets import QApplication
+# from gui import MineMap
+from receiver import Connection
+from config import ESP_IP, ESP_PORT
+import asyncio
 
 
-app = QApplication(sys.argv)
-window = MineMap()
-window.show()
+async def main():
+    connection = Connection(ESP_IP, ESP_PORT)
+    await connection.run()
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
+# app = QApplication(sys.argv)
+# window = MineMap()
+# window.show()
--- client/receiver.py
+++ client/receiver.py
@@ -2,35 +2,58 @@ import asyncio
 import websockets
 import json
 
+
 class Connection:
     def __init__(self, esp_ip, esp_port):
-        self.esp_ip = esp_ip
-        self.esp_port = esp_port
+        self.uri = f"ws://{esp_ip}:{esp_port}"
+        self.data_queue = asyncio.Queue()
 
-    async def connect(self):
-        return await websockets.connect()
+    async def __connect(self):
+        self.websocket = await websockets.connect(self.uri)
+        print(f"Connected to {self.uri}")
 
-    async def receive_data(self):    
+    async def __receive_data(self):
         try:
             async for request in self.websocket:
-                print(f"Request received: {request}")
-                self.requests_queue.put_nowait(request)
+                # print(f"Request received: {request}")
+                self.data_queue.put_nowait(request)
         except websockets.exceptions.ConnectionClosed:
             print("Websocket was closed")
-            await self.disconnect()
+            await self.websocket.close()
             return
         except asyncio.CancelledError:
             print("Handler task was cancelled")
             return
-    
+
+    async def __process_data (self):
+        while True:
+            try:
+                data = await self.data_queue.get()
+                print(f"Processing data: {data}")
+            except asyncio.CancelledError:
+                print("Data processing task was cancelled")
+                break
+
     async def send_data(self, data: dict):
         try:
             data = json.dumps(data).encode('utf-8')
             await self.websocket.send(data)
         except websockets.exceptions.ConnectionClosed:
             print("Websocket was closed")
-            await self.disconnect()
+            await self.websocket.close()
             return
         except asyncio.CancelledError:
             print("Handler task was cancelled")
             return
+
+    async def run(self):
+        await self.__connect()
+        receive_task = asyncio.create_task(self.__receive_data())
+        process_task = asyncio.create_task(self.__process_data())
+
+        try:
+            await asyncio.gather(receive_task, process_task)
+        except asyncio.CancelledError:
+            print("Run task was cancelled")
+        finally:
+            await self.websocket.close()
